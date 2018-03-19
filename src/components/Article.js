198c835c348e718d8461bf15ede505682f2e9111
Michael Manges <michael.manges@gmail.com>
2018-03-19 13:29:22 +0400

    Hard coded Article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,8 +5,21 @@ class Article
 extends Component {
   render() {
     return (
-      <div>
+      <div className="article">
         <h4 className="article__heading">Today</h4>
+        <div className="article__grid">
+          <a href="#" target="_blank" className="article__comments">
+            <h3 className="count">▲ 3</h3>
+            <p className="comments"><span role="img" aria-label="talking bubble">💬</span> 0</p>
+          </a>
+          <h3 className="article__title">
+            <a href="#" target="_blank">Article Title</a>
+          </h3>
+          <p className="article__desc">
+            This will be a brief description of the article.
+          </p>
+          <p className="article__time">3 hours ago by <a href="#" className="article__author">someauthor</a></p>
+        </div>
       </div>
     );
   }
